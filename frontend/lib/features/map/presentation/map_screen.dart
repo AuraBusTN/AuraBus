705dad4849534df955c5289888c62e6ad1a8aa1d
Ceralex <alessandroceresa05@gmail.com>
2025-12-12 16:08:01 +0100

    feat: add current user position with gps

--- frontend/lib/features/map/presentation/map_screen.dart
+++ frontend/lib/features/map/presentation/map_screen.dart
@@ -13,11 +13,30 @@ class MapScreen extends ConsumerStatefulWidget {
 }
 
 class _MapScreenState extends ConsumerState<MapScreen>
-    with AutomaticKeepAliveClientMixin {
+    with AutomaticKeepAliveClientMixin, WidgetsBindingObserver {
   @override
   bool get wantKeepAlive => true;
 
   final LatLng _center = const LatLng(46.0678, 11.1303);
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      ref.read(mapControllerProvider).onAppResumed();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +45,6 @@ class _MapScreenState extends ConsumerState<MapScreen>
     final mapController = ref.read(mapControllerProvider);
     final markersAsync = ref.watch(markersProvider);
     final stopsMap = ref.watch(stopsMapProvider);
-
     final mapStyle = ref.read(mapStyleProvider).value;
 
     return markersAsync.when(
@@ -43,14 +61,21 @@ class _MapScreenState extends ConsumerState<MapScreen>
           );
         }).toSet();
 
-        return GoogleMap(
-          key: const Key('google_map'),
-          initialCameraPosition: CameraPosition(target: _center, zoom: 13),
-          onMapCreated: mapController.onMapCreated,
-          style: mapStyle,
-          markers: markers,
-          zoomControlsEnabled: true,
-          mapType: MapType.normal,
+        return ValueListenableBuilder<bool>(
+          valueListenable: mapController.showLocation,
+          builder: (context, showBlueDot, child) {
+            return GoogleMap(
+              key: const Key('google_map'),
+              initialCameraPosition: CameraPosition(target: _center, zoom: 13),
+              onMapCreated: mapController.onMapCreated,
+              style: mapStyle,
+              markers: markers,
+              zoomControlsEnabled: true,
+              mapType: MapType.normal,
+              myLocationEnabled: showBlueDot,
+              myLocationButtonEnabled: showBlueDot,
+            );
+          },
         );
       },
       loading: () => const Center(child: CircularProgressIndicator()),
