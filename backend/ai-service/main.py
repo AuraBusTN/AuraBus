f0edb00ca6310c1caed261585ab87947c5238a9f
LUCiv175 <luca.civera@outlook.com>
2026-02-06 18:03:13 +0100

    feat: add AI service with FastAPI for trip delay predictions
    - Implemented main application logic in main.py to handle predictions using XGBoost.
    - Defined data models for incoming requests using Pydantic.
    - Added requirements.txt for necessary dependencies including FastAPI, XGBoost, and others.
    - Updated docker-compose.yaml to include the AI service, enabling it to run alongside the MongoDB service.

---
+++ backend/ai-service/main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import xgboost as xgb
+import pandas as pd
+import os
+
+MODEL_PATH = 'aurabus_brain_v1.json'
+
+
+class StopInfo(BaseModel):
+    stop_encoded: int
+    hour: int
+    day_of_week: int
+    directionId: int
+
+
+class TripRequest(BaseModel):
+    route_encoded: int
+    current_delay: int
+    future_stops: List[StopInfo]
+
+
+model = xgb.XGBRegressor()
+is_model_loaded = False
+
+if os.path.exists(MODEL_PATH):
+    model.load_model(MODEL_PATH)
+    is_model_loaded = True
+    print('✅ Brain loaded! I\'m ready to predict.')
+else:
+    print('⚠️ WARNING: Brain file not found. Predictions will not work.')
+
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return {'status': 'OK', 'message': 'AuraBus AI is alive!'}
+
+
+@app.post('/predict')
+def predict(request: TripRequest):
+    if not is_model_loaded:
+        return {'error': 'Model not loaded. Cannot make predictions.'}
+
+    predictions = []
+
+    running_delay = request.current_delay
+
+    for stop in request.future_stops:
+        feature_names = ['route_encoded', 'stop_encoded',
+                         'directionId', 'hour', 'day_of_week', 'delay']
+
+        # 2. Creiamo il DataFrame
+        input_data = pd.DataFrame([[
+            request.route_encoded,
+            stop.stop_encoded,
+            stop.directionId,
+            stop.hour,
+            stop.day_of_week,
+            running_delay
+        ]], columns=feature_names)
+
+        prediction = model.predict(input_data, validate_features=False)
+
+        predicted_delay = int(prediction[0])
+
+        predictions.append({
+            'stop_encoded': stop.stop_encoded,
+            'predicted_delay': predicted_delay
+        })
+
+        running_delay = predicted_delay
+
+    return {'predictions': predictions}
